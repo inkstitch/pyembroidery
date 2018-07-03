bdef6b4a79e068c27a06c44b8b70768f1e5bdb1f
tatarize <tatarize@gmail.com>
2018-07-03 04:43:01 -0700

    Delete StitchBlocks.py

--- pyembroidery/StitchBlocks.py
+++
@@ -1,7 +0,0 @@
-class StitchBlocks:
-    def __init__(self):
-        self.stitchblocks = []
-
-    def convert_from_pattern(self, pattern):
-        for stitchblock in pattern.get_as_stitchblocks():
-            self.stitchblocks.append(stitchblock)
