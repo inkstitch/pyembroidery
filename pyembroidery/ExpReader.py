7069675b58febea99626055b6d3581746192aae0
tatarize <tatarize@gmail.com>
2018-07-07 20:26:39 -0700

    Fixed EXP writing.
    Doesn't do anything weird or generate any odd stitch stuff.

--- pyembroidery/ExpReader.py
+++ pyembroidery/ExpReader.py
@@ -1,4 +1,3 @@
-
 def signed(b):
     if b > 127:
         return -256 + b
@@ -13,19 +12,20 @@ def read(f, out, settings=None):
             break
         if b[0] & 0xFF == 0x80:
             if b[1] == 0x80:  # Trim
-                b = bytearray(f.read(2))
+                b = bytearray(f.read(2))  # 07 00
                 if len(b) != 2:
                     break
                 out.trim(0, 0)
             elif b[1] == 0x02:
                 out.stitch(signed(b[0]), -(signed(b[1])))
+                # This shouldn't exist.
             elif b[1] == 0x04:  # Jump
                 b = bytearray(f.read(2))
                 if len(b) != 2:
                     break
                 out.move(signed(b[0]), -signed(b[1]))
             elif b[1] == 0x01:  # Colorchange
-                b = bytearray(f.read(2))
+                b = bytearray(f.read(2))  # 00 00
                 if len(b) != 2:
                     break
                 out.color_change(0, 0)
--- pyembroidery/ExpWriter.py
+++ pyembroidery/ExpWriter.py
@@ -7,7 +7,6 @@ MAX_STITCH_DISTANCE = 127
 
 def write(pattern, f, settings=None):
     stitches = pattern.stitches
-    jumping = False
     xx = 0
     yy = 0
     for stitch in stitches:
@@ -17,34 +16,22 @@ def write(pattern, f, settings=None):
         dx = x - xx
         dy = y - yy
         if data is STITCH:
-            if jumping:
-                f.write(b'\x00\x00')
-                jumping = False
             # consider bounds checking the delta_x, delta_y and raising ValueError if exceeds.
             delta_x = int(round(dx)) & 0xFF
             delta_y = -int(round(dy)) & 0xFF
             f.write(bytes(bytearray([delta_x, delta_y])))
         elif data == JUMP:
-            jumping = True
             delta_x = int(round(dx)) & 0xFF
             delta_y = -int(round(dy)) & 0xFF
             f.write(b'\x80\x04')
             f.write(bytes(bytearray([delta_x, delta_y])))
         elif data == TRIM:
             f.write(b'\x80\x80\x07\x00')
-            f.write(b'\x80\x04\x00\x00')
             continue
         elif data == COLOR_CHANGE:
-            if jumping:
-                f.write(b'\x00\x00')
-                jumping = False
             f.write(b'\x80\x01\x00\x00')
-            f.write(b'\x80\x80\x07\x00')
             continue
         elif data == STOP:
-            if jumping:
-                f.write(b'\x00\x00')
-                jumping = False
             f.write(b'\x80\x01\x00\x00')
             continue
         elif data == END:
