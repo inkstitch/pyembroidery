4d7ccb7e40784d578f11b52e375ef36bef421ad9
Lex Neva <github.com@lexneva.name>
2018-10-15 19:41:16 -0400

    use windows-style line endings

--- pyembroidery/GcodeWriter.py
+++ pyembroidery/GcodeWriter.py
@@ -21,15 +21,15 @@ def write(pattern, f, settings=None):
     width = extents[2] - extents[0]
     height = extents[3] - extents[1]
 
-    write_string_utf8(f, '(STITCH_COUNT:%d)\n' % pattern.count_stitches())
-    write_string_utf8(f, '(EXTENTS_LEFT:%.3f)\n' % extents[0])
-    write_string_utf8(f, '(EXTENTS_TOP:%.3f)\n' % extents[1])
-    write_string_utf8(f, '(EXTENTS_RIGHT:%.3f)\n' % extents[2])
-    write_string_utf8(f, '(EXTENTS_BOTTOM:%.3f)\n' % extents[3])
-    write_string_utf8(f, '(EXTENTS_WIDTH:%.3f)\n' % width)
-    write_string_utf8(f, '(EXTENTS_HEIGHT:%.3f)\n' % height)
-    write_string_utf8(f, "\n")
-    write_string_utf8(f, "G0 X0.0 Y0.0\n")
+    write_string_utf8(f, '(STITCH_COUNT:%d)\r\n' % pattern.count_stitches())
+    write_string_utf8(f, '(EXTENTS_LEFT:%.3f)\r\n' % extents[0])
+    write_string_utf8(f, '(EXTENTS_TOP:%.3f)\r\n' % extents[1])
+    write_string_utf8(f, '(EXTENTS_RIGHT:%.3f)\r\n' % extents[2])
+    write_string_utf8(f, '(EXTENTS_BOTTOM:%.3f)\r\n' % extents[3])
+    write_string_utf8(f, '(EXTENTS_WIDTH:%.3f)\r\n' % width)
+    write_string_utf8(f, '(EXTENTS_HEIGHT:%.3f)\r\n' % height)
+    write_string_utf8(f, "\r\n")
+    write_string_utf8(f, "G0 X0.0 Y0.0\r\n")
 
     z = 0
     for i, (x, y, command) in enumerate(pattern.stitches):
@@ -44,12 +44,12 @@ def write(pattern, f, settings=None):
             x /= 10.0
             y /= 10.0
 
-            write_string_utf8(f, "G0 X%.3f Y%.3f\n" % (x, y))
+            write_string_utf8(f, "G0 X%.3f Y%.3f\r\n" % (x, y))
 
             # Stitching is modelled as continuous travel on the Z axis.  The
             # Z motor is hooked up to the hand wheel of the sewing machine.
             z += stitch_z_travel
-            write_string_utf8(f, "G0 Z%.1f\n" % z)
+            write_string_utf8(f, "G0 Z%.1f\r\n" % z)
 
-    write_string_utf8(f, "\n")
-    write_string_utf8(f, "M30\n")
+    write_string_utf8(f, "\r\n")
+    write_string_utf8(f, "M30\r\n")
