da4bd9ff669243ae56fda548b4e56dfa57a5e654
Lex Neva <github.com@lexneva.name>
2019-03-06 20:44:32 -0500

    bump version to 1.2.32

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyembroidery",
-    version="1.2.31",
+    version="1.2.32",
     author="Tatarize",
     author_email="tatarize@gmail.com",
     description="Embroidery IO library",
