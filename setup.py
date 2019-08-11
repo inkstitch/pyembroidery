ce7771276aaaadaa184db5fd2ac19610d8941077
Lex Neva <github.com@lexneva.name>
2019-08-10 21:40:11 -0400

    1.2.38

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyembroidery",
-    version="1.2.37",
+    version="1.2.38",
     author="Tatarize",
     author_email="tatarize@gmail.com",
     description="Embroidery IO library",
